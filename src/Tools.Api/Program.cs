b13ddcfec51bc6995712b1c2394e7d235c54fdf3
igeekfan <luoyunchong@foxmail.com>
2023-03-06 00:18:25 +0800

    Add 二级目录，方便单域名部署

--- src/Tools.Api/Program.cs
+++ src/Tools.Api/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Tools.Api;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+var c = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
@@ -14,18 +14,19 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services
-    .AddSwagger(configuration)
-    .AddCustomMVC(configuration);
+    .AddSwagger(c)
+    .AddCustomMVC(c);
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
+string vPath = c.GetValue("VirtualPath", "");
+app.UsePathBase(new PathString(vPath));
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint($"{vPath}/swagger/v1/swagger.json", "Tools API");
+});
 
 app.UseStaticFiles();
 
